38b64c138e697838ed992d101ac536590686e5c2
AbdAlluhHabeb <haliafhl83@gmail.com>
2025-12-18 14:21:56 +0200

    Create Reset Password and Forget Password pages

--- src/app/(auth)/(auth-proses)/forget-password/page.jsx
+++ src/app/(auth)/(auth-proses)/forget-password/page.jsx
@@ -1,17 +1,16 @@
 "use client";
 
 import { useState } from "react";
-import { IoIosLock } from "react-icons/io";
+import { GoMail } from "react-icons/go";
 import { useRouter } from "next/navigation";
 import { useLoginMutation } from "@/redux/auth/authAPI";
 import { useDispatch, useSelector } from "react-redux";
 import { loginSuccess, loginFailure } from "@/redux/auth/authSlice";
 import {RiUserSettingsLine} from "@remixicon/react";
 
-function ResetPasswordPage() {
+function ForgetPasswordPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [rememberMe, setRememberMe] = useState(false);
     const [login, { isLoading }] = useLoginMutation();
     const { error } = useSelector((state) => state.auth);
     const dispatch = useDispatch();
@@ -42,10 +41,10 @@ function ResetPasswordPage() {
 
                 <div className="flex flex-col items-center justify-center gap-2 text-center">
                     <sapn className="text-2xl text-gray-900">
-                        {`Reset password`}
+                        {`Forget Password`}
                     </sapn>
                     <span className="text-sm text-gray-500">
-            {'Enter your new password'}
+            {'Enter your details to reset passowrd.'}
           </span>
                 </div>
 
@@ -53,28 +52,14 @@ function ResetPasswordPage() {
                     <div className="flex flex-col gap-2 w-full">
 
                         <div className={"flex flex-col gap-2 w-full"}>
-                            <label>New Password</label>
+                            <label>Email Address</label>
                             <div className="flex bg-white pl-2 px-2 w-full items-center border-2 rounded-xl">
-                                <IoIosLock className="text-gray-500 w-10" size={18} />
+                                <GoMail className="text-gray-500 w-10" size={18} />
                                 <input
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    placeholder="*"
-                                    className="w-full py-3 px-2 outline-none"
-                                    required
-                                />
-                            </div>
-                        </div>
-                        <div className={"flex flex-col gap-2 w-full"}>
-                            <label>Confirm New Password</label>
-                            <div className="flex bg-white pl-2 px-2 w-full items-center border-2 rounded-xl">
-                                <IoIosLock className="text-gray-500 w-10" size={18} />
-                                <input
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    placeholder="*"
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="Enter your email"
                                     className="w-full py-3 px-2 outline-none"
                                     required
                                 />
@@ -88,7 +73,7 @@ function ResetPasswordPage() {
                             disabled={isLoading}
                             className="w-full rounded-lg bg-primary-base text-white py-1.5"
                         >
-                            {isLoading ? "Loading..." : "Reset"}
+                            {isLoading ? "Loading..." : "Send"}
                         </button>
                     </div>
                 </div>
@@ -97,4 +82,4 @@ function ResetPasswordPage() {
     );
 }
 
-export default ResetPasswordPage;
+export default ForgetPasswordPage;
