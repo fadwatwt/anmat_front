95d2f2d18a98962bd0e30b912c597529cae32c45
Saifeddeen <20173065@student.alazhar.edu.ps>
2026-01-31 16:46:57 +0300

    Bind Subscriber Role APIs: Create, List, Sync Permissions, Delete

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -16,6 +16,6 @@ export const apiSlice = createApi({
     }),
     // Initialize generic tag types here. 
     // Features can add their own tags later (e.g. 'Projects', 'Tasks')
-    tagTypes: ["User", "Auth", "SystemAdmins", "AdminRoles", "AdminPermissions", "Industries", "SubscriptionPlans", "SubscriptionFeatureTypes", "Subscriptions", "Organizations", "MoneyReceivingMethods", "Positions", "Departments", "Employees", "Attendances", "Leaves", "Salaries", "EmployeeRequests", "Permissions"],
+    tagTypes: ["User", "Auth", "SystemAdmins", "AdminRoles", "AdminPermissions", "Industries", "SubscriptionPlans", "SubscriptionFeatureTypes", "Subscriptions", "Organizations", "MoneyReceivingMethods", "Positions", "Departments", "Employees", "Attendances", "Leaves", "Salaries", "EmployeeRequests", "Permissions", "Roles"],
     endpoints: () => ({}), // Start empty, inject endpoints in other files
 });
