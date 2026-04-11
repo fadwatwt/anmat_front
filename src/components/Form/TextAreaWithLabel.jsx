b237392eac3338d27a7a1ac6755ed6aad30849d4
Saifeddeen <20173065@student.alazhar.edu.ps>
2026-04-11 15:32:06 +0300

    Updates: Department Required Fields | Theming Requests

--- src/components/Form/TextAreaWithLabel.jsx
+++ src/components/Form/TextAreaWithLabel.jsx
@@ -1,13 +1,14 @@
 import { useTranslation } from "react-i18next";
 import PropTypes from "prop-types";
 
-function TextAreaWithLabel({ title, placeholder, className, value, name, onChange, rows = 6, maxLength, isOptional = false, error, onBlur }) {
+function TextAreaWithLabel({ title, placeholder, className, value, name, onChange, rows = 6, maxLength, isOptional = false, isRequired = false, error, onBlur, ...rest }) {
     const { t } = useTranslation()
 
     return (
         <div className={`flex flex-col gap-1 w-full items-start  ${className}`}>
             <label className={"text-cell-primary text-sm font-medium"}>
                 {t(title)}
+                {isRequired && <span className={"text-red-500"}>*</span>}
                 {isOptional && <span className="text-secondary font-normal mx-1">({t("Optional")})</span>}
             </label>
             <textarea
@@ -19,6 +20,7 @@ function TextAreaWithLabel({ title, placeholder, className, value, name, onChang
                 maxLength={maxLength}
                 placeholder={`${t(placeholder)}...`}
                 className={`py-3 px-2 text-sm bg-status-bg border-status-border border-2 rounded-xl w-full focus:outline-none focus:border-primary-400 text-cell-primary placeholder:text-cell-secondary/50 resize-none ${error ? 'border-red-500' : ''}`}
+                {...rest}
             >
             </textarea>
             <div className="w-full flex justify-between items-center">
@@ -45,6 +47,7 @@ TextAreaWithLabel.propTypes = {
     rows: PropTypes.number,
     maxLength: PropTypes.number,
     isOptional: PropTypes.bool,
+    isRequired: PropTypes.bool,
     error: PropTypes.string
 }
 
