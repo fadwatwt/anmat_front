ac54b2db670b6003424f13bf395ebf0a196c8d12
AbdAlluhHabeb <haliafhl83@gmail.com>
2026-07-08 14:15:49 +0300

    Update root file to live

--- src/Root.Route.js
+++ src/Root.Route.js
@@ -1,5 +1,5 @@
 // export const RootRoute = "https://anmat-backend-system.onrender.com";
-export const RootRoute = "httpss://f53jcyal5or35jvazzm5n1zj.2.24.161.24.sslip.io";
+export const RootRoute = "https://api.anmaat.com";
 // export const RootRoute = "http://127.0.0.1:3000";
 // export const RootRoute = "https://anmat-backend-system-2.onrender.com";
 export const ExternalServer = "http://localhost:8000/api/v1";
